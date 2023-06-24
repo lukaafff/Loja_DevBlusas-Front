3a3fe9b5d1a1e85d88fc45000920884795b423f1
lukaafff <luizagarcia2405@gmail.com>
2023-06-24 17:10:18 -0300

    card de roupa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import Cards from "./components/Cards/Cards";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Cards/>
       <Footer/>
     </div>
   );
