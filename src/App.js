8b47cb5c9657aa6e7f203e38d1b5213434464e0a
Rafael Junges <rafadevweb@gmail.com>
2023-06-28 15:52:45 -0300

    introdução do menu de atendimento

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 
 import { Outlet } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import CentralAtendimento from "./components/CentralAtendimento";
 import"./App.css";
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+
+      {/* 
       <div className="container">
         <Outlet/>
-      </div>
+      </div> COMENTÁRIO JSX */}
+      <CentralAtendimento />
+      <Footer/>
     </div>
   );
 }
