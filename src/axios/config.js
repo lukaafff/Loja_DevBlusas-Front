6a7ed6c595210d0243f5a811e56cda8118da861a
Rafael Junges <rafadevweb@gmail.com>
2023-07-10 21:24:16 -0300

    Demonstra qual port esta buscando a api

--- src/axios/config.js
+++ src/axios/config.js
@@ -1,10 +1,13 @@
 import axios from "axios";
 
+const port = 4000
 const siteFetch = axios.create({
-    baseURL: "http://localhost:4444",
+    baseURL: `http://localhost:${port}`,
     headers: {
         "Content-Type": "aplication/json"
     }
 });
+
+console.log(`Server rodando na porta ${port}` )
 
 export default siteFetch;
