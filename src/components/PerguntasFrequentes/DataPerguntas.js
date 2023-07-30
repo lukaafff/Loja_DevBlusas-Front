b17dd808e682df207da1ffe43e7728d82b622c7b
lukaafff <luizagarcia2405@gmail.com>
2023-07-30 19:26:12 -0300

    perguntas frequentes

---
+++ src/components/PerguntasFrequentes/DataPerguntas.js
@@ -0,0 +1,28 @@
+//armazenar todos os links de navegação a partir de uma lista
+export const DataPerguntas = [
+    {
+        title: "Como faço para comprar?",
+        desc: "As nossas vendas são realizadas somente pelo site. É preciso fazer um cadastro para realizar a compra. Por favor, coloque seu dados e o endereço corretamente.",
+        cName: "perguntasDrop"
+    },
+    {
+        title: "Quando meu pedido será enviado?",
+        desc: "Os pedidos aprovados são postados em até 1 dia útil.",
+        cName: "perguntasDrop"
+    },
+    {
+        title: "Como sei meu tamanho??",
+        desc: "Nós trabalhamos com tamanho único, que serve numeração 38 ao 46. Para conferir as medidas exatas, consulte a nossa tabela de medidas.",
+        cName: "perguntasDrop"
+    },
+    {
+        title: "Como sei que esse site é confiável?",
+        desc: "Para a sua conferência, acesse o nosso instagram @devblusas.oficial Lá você pode conferir nos nossos stories salvos as mensagens de inúmeras clientes nossas, fotos delas usando as nossas peças e também tem registros de nossos pacotes sendo enviados.",
+        cName: "perguntasDrop"
+    },
+    {
+        title: "Não estou conseguindo fazer pagamento por pix",
+        desc: "O código que é gerado pelo sistema, NÃO É uma Chave PIX. Esse código deve ser utilizado na opção COPIA E COLA no seu aplicativo bancário.",
+        cName: "perguntasDrop"
+    }
+]
